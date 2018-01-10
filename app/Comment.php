97429db7ef0b254bd711b0c2d4f01f1e6294e83a
Ilya <sleptsovilya@gmail.com>
2018-01-10 14:24:47 +0300

    First commit

---
+++ app/Comment.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    //
+    public function post()
+    {
+        return $this->hasOne(Post::class);
+    }
+
+    public function author()
+    {
+        return $this->hasOne(User::class);
+    }
+}
