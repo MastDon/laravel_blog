a80dcf25ffd005e29eccbdcfdd8fedbe07293c1c
Ilya <sleptsovilya@gmail.com>
2018-01-10 17:53:34 +0300

    Edit method Post and User

--- app/Post.php
+++ app/Post.php
@@ -4,10 +4,16 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Support\Facades\Storage;
 
 class Post extends Model
 {
     use Sluggable;
+
+    const IS_DRAFT = 0;
+    const IS_PUBLIC = 1;
+
+    protected $fillable = ['title', 'content'];
 
     public function category()
     {
@@ -41,11 +47,120 @@ class Post extends Model
         ];
     }
 
-    public static function add($title, $content)
+    public static function add($fields)
     {
 
+        $post = new static;
+        $post->fill($fields);
+        $post->user_id = 1;
+        $post->save();
+
+        return $post;
+    }
+
+    public function edit($fields)
+    {
+        $this->fill($fields);
+        $this->save();
 
     }
 
+    public function remove()
+    {
+        Storage::delete('uploads/' . $this->image);
+        $this->delete();
+    }
+
+
+    public function getImage()
+
+    {
+        if ($this->image == null) {
+            return '/img/no-image.png';
+        }
+        return '/uploads' . $this->image;
+    }
+
+
+    public function uploadImage($image)
+    {
+        if ($image == null) {
+            return;
+        }
+        Storage::delete('uploads/' . $this->image);
+        $filename = str_random(10) . '.' . $image->extension();
+        $image->saveAs('uploads', $filename);
+        $this->image = $filename;
+        $this->save();
+
+    }
+
+    public function setCategory($id)
+    {
+        if ($id == 0) {
+            return;
+        }
+        $this->category_id = $id;
+        $this->save();
+
+    }
+
+    public function setTags($ids)
+    {
+        if ($ids == 0) {
+            return;
+        }
+        $this->tags()->sync($ids);
+    }
+
+    //Устанвока статуса "В черновик" или в "Публичный доступ"
+    public function setDraft()
+    {
+        $this->status = Post::IS_DRAFT;
+        $this->save();
+
+    }
+
+    public function setPublic()
+    {
+
+        $this->status = Post::IS_PUBLIC;
+        $this->save();
+    }
+
+    public function toggleStatus($value)
+    {
+        if ($value == null) {
+            return $this->setDraft();
+        }
+        return $this->setPublic();
+
+    }
+
+    //Установка рекомендованных статей
+
+    public function setFeatured()
+    {
+        $this->is_featured = 1;
+        $this->save();
+
+    }
+
+    public function setStandart()
+    {
+
+        $this->is_featured = 0;
+        $this->save();
+    }
+
+    //Переключатель дл ярекомендованных статей
+    public function toggleFeatured($value)
+    {
+        if ($value == null) {
+            return $this->setStandart();
+        }
+        return $this->setFeatured();
+
+    }
 
 }
