23380616e06857694820dad6fdb7a3dfabf09032
Ilya <sleptsovilya@gmail.com>
2018-02-02 16:45:43 +0300

    Add post

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
+use App\Category;
+use App\Tag;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('pages.index');
+        $posts=Post::paginate(2);
+        return view('pages.index',['posts'=> $posts]);
     }
 }
